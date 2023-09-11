8273c3a6eb6b4402b497a07018bee29ada6c3854
Ylellan <jamely.ortega@gmail.com>
2023-09-11 13:14:13 -0500

    hola

---
+++ src/Practicas/calculoHipotenusa.java
@@ -0,0 +1,10 @@
+package Practicas;
+
+public class calculoHipotenusa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Practicas/calculosuma.java
@@ -0,0 +1,10 @@
+package Practicas;
+
+public class calculosuma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
